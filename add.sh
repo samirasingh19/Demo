d656650e41139bf1ac2508b6e68309db0aa50a3a
samirasingh19 <samira.singh021@gmail.com>
2023-02-27 22:13:16 +0530

    [Samira]: Added practice problems

---
+++ add.sh
@@ -0,0 +1,6 @@
+read -p "Enter 1st number:" x
+read -p "Enter 2nd number:" y
+echo $x
+echo $y
+z= $(($x + $y))
+echo $z
---
+++ if.sh
@@ -0,0 +1,6 @@
+read -p "Enter the number:" a
+read -p "enter the number:" b
+if [ $a -gt $b ]
+then
+echo $a is greater than $b
+fi
